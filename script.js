814853d5df712bcaa27698acb6c19cfbd0a5b012
Linssi Colin Ruiz <linssi101@gmail.com>
2023-10-03 08:22:13 -0500

    modified the shapes

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ window.addEventListener('load', function () {
   const ctx = canvas.getContext('2d');
   canvas.width = 600;
   canvas.height = 600;
+  // canvas settings
+  console.log(ctx);
+  ctx.lineWidth = 50;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = 'green';
+  ctx.fillStyle='blue'
 
   class Fractal {
     constructor(canvasWidth, canvasHeight) {
       this.canvasWidth = canvasWidth;
       this.canvasHeight = canvasHeight;
+      this.size = this.canvasWidth * 0.4;
     }
     draw(context) {
+      context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
       context.beginPath();
       context.moveTo(0, 0);
-      context.lineTo(canvas.width, canvas.height);
+      context.lineTo(this.size, 0);
       context.stroke();
     }
   }
