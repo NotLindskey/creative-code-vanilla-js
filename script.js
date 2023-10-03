d3d328cd4c598f19e5b59dca2fba1b601526913b
Linssi Colin Ruiz <linssi101@gmail.com>
2023-10-03 08:13:52 -0500

    drawing basic shapes

--- script.js
+++ script.js
@@ -3,4 +3,24 @@ window.addEventListener('load', function () {
   const ctx = canvas.getContext('2d');
   canvas.width = 600;
   canvas.height = 600;
+
+  class Fractal {
+    constructor(canvasWidth, canvasHeight) {
+      this.canvasWidth = canvasWidth;
+      this.canvasHeight = canvasHeight;
+    }
+    draw(context) {
+      context.beginPath();
+      context.moveTo(0, 0);
+      context.lineTo(canvas.width, canvas.height);
+      context.stroke();
+    }
+  }
+
+  const fractal1 = new Fractal(canvas.width, canvas.height);
+  fractal1.draw(ctx);
+
+  class Particle {}
+
+  class Rain {}
 });
